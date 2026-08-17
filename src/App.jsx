baaa5d7cc6f22d6e842bc39a197283623909bf71
Cyoku-ell <kokuofficial1@gmail.com>
2026-08-17 18:33:43 +0800

    function

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,22 +10,37 @@ function App() {
   const umur = 17;
   const pendidikan = "SMK 1 BALIKPAPAN";
   const jurusan = "XII RPL";
+  const angka1 = 12
+  const angka2 = 4
+
+  const kali = (angka1, angka2) => {
+    return angka1 * angka2;
+  };
+
+  const hasilkali = kali(angka1, angka2);
+
+  const tambah = (angka1, angka2) => {
+    return angka1 + angka2;
+  };
+
+  const hasiltambah = tambah(angka1,angka2);
+
+  const kurang = (angka1, angka2) => {
+    return angka1 - angka2;
+  };
+
+  const hasilkurang = kurang(angka1, angka2)
+  
 
   return (
     <>
       <h1>Belajar React with {nama}:D</h1>
       <br></br>
-      <p>Aku baru mulai hari ini</p>
-      <br></br>
-      <h2>
-        Biodata:
-        <br></br>
-        =============
-      </h2>
-      <p>Nama : {nama}</p>
-      <p>Umurku : {umur} Tahun!</p>
-      <p>Pendidikan : {pendidikan}</p>
-      <p>Jurusan : {jurusan}</p>
+      <p1>Angka 1 : {angka1} </p1>
+      <p1>Angka 2 : {angka2} </p1>
+      <p>Hasil kali = {hasilkali}</p>
+      <p>Hasil tambah = {hasiltambah}</p>
+      <p>Hasil kurang = {hasilkurang}</p>
     </>
   );
 }
